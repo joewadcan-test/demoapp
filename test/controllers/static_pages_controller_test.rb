f214a48e1fbf95c2cbac79cc0f613e541877c39a
joe_w <joe+test@wadcan.com>
2016-09-17 22:21:00 +0000

    added home help tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    assert_select "h1", "DemoApp"
     assert_response :success
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "h1", "Help"
   end
 
 end
